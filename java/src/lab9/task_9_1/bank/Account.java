54685bfd4ec8fab2cb325fdaec2ce35127a644d4
IvanOmelchenkoIP <io25112002@gmail.com>
2023-01-02 21:13:52 +0200

    Add locks
    Add locks for multithreading in accounts

--- java/src/lab9/task_9_1/bank/Account.java
+++ java/src/lab9/task_9_1/bank/Account.java
@@ -1,11 +1,22 @@
 package lab9.task_9_1.bank;
 
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock.ReadLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock.WriteLock;
+
 public class Account {
 
 	private int balance;
 	
+	private ReentrantReadWriteLock accountLock;
+	private ReadLock readLock;
+	private WriteLock writeLock;
+	
 	public Account(int initBalance) {
 		this.balance = initBalance;
+		this.accountLock = new ReentrantReadWriteLock();
+		this.readLock = this.accountLock.readLock();
+		this.writeLock = this.accountLock.writeLock();
 	}
 	
 	public void withdraw(int amount) {
@@ -17,6 +28,11 @@ public class Account {
 	}
 	
 	public int getBalance() {
-		return balance;
+		readLock.lock();
+		try{
+			return balance;		
+		} finally {
+			readLock.unlock();
+		}
 	}
 }
