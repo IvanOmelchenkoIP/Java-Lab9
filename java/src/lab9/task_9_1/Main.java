9c06ba7d4539774f7f336d4a54260ffdb318cf1b
IvanOmelchenkoIP <io25112002@gmail.com>
2023-01-02 22:37:09 +0200

    Implement threads to main
    Implement thread usage to main

--- java/src/lab9/task_9_1/Main.java
+++ java/src/lab9/task_9_1/Main.java
@@ -2,48 +2,47 @@ package lab9.task_9_1;
 
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.concurrent.CountDownLatch;
 
 import lab9.task_9_1.bank.Account;
 import lab9.task_9_1.bank.Bank;
 import lab9.task_9_1.bank.BankAccountsManager;
+import lab9.task_9_1.transfer.TransferManager;
 
 public class Main {
 
 	public static void main(String[] args) {		
-		//Account a1 = new Account(200);
-		//Account a2 = new Account(300);
-		
 		final int ACCOUNTS_AMOUNT = 50;
 		final int ACCOUNT_MAX_INIT_BALANCE = 500;
-		final int THREAD_AMOUNT = 200;
+		final int MAX_TRANSACTION = 250;
+		final int THREAD_AMOUNT = 10000;
 		
 		Random random = new Random();
 		
-		BankAccountsManager manager = new BankAccountsManager(new Bank());
+		BankAccountsManager accountsManager = new BankAccountsManager(new Bank());
 		ArrayList<Account> accounts = new ArrayList<Account>();
-		/*for (int i = 0; i < ACCOUNTS_AMOUNT; i++) {
-			accounts.add(manager.createAccount(random.nextInt(ACCOUNT_MAX_INIT_BALANCE)));
-		}*/
+		for (int i = 0; i < ACCOUNTS_AMOUNT; i++) {
+			accounts.add(accountsManager.createAccount(random.nextInt(ACCOUNT_MAX_INIT_BALANCE)));
+		}
+		int initTotalBalance = accountsManager.getTotalAccountBalance();		
 		
-		int initTotalBalance = manager.getTotalAccountBalance();		
-		System.out.println(initTotalBalance);
+		CountDownLatch barrier = new CountDownLatch(THREAD_AMOUNT);
+		TransferManager transferManager = new TransferManager(accounts, accountsManager, barrier);
+		for (int i = 0; i < THREAD_AMOUNT; i++) {
+			Account from = accounts.get(random.nextInt(ACCOUNTS_AMOUNT));
+			Account to = accounts.get(random.nextInt(ACCOUNTS_AMOUNT));
+			int amount = random.nextInt(MAX_TRANSACTION);
+			new Thread(transferManager.newTransfer(from, to, amount)).start();
+		}
+		try {
+			barrier.await();
+			int transferTotalBalance = accountsManager.getTotalAccountBalance();
 		
-		/*int accountNumber = 20;
-		int maxMoney = 500;
-		Random random = new Random();
-		int moneyAmount = 0;
-		ArrayList<Account> accounts = new ArrayList<Account>();
-		for (int i = 0; i < accountNumber; i++) {
-			int money = random.nextInt(maxMoney);	
-			moneyAmount += money;
-			accounts.add(new Account(money));
-		}		
-		ArrayList<Thread> threads = new ArrayList<Thread>();
-		for (int i = 0; i < 3000; i++) threads.add(new Thread());
-		//bank.transfer(a1, a2, 30);
-		
-		System.out.println(moneyAmount);*/
-		//System.out.println(a2.getMoney());
+			System.out.println("Initial balance: " + initTotalBalance);
+			System.out.println("Balance after transactions: " + transferTotalBalance);
+		} catch (InterruptedException ex) {
+			System.out.println(ex.getMessage());
+		}
 	}
 
 }
--- java/src/lab9/task_9_1/transfer/TransferManager.java
+++ java/src/lab9/task_9_1/transfer/TransferManager.java
@@ -1,38 +1,29 @@
 package lab9.task_9_1.transfer;
 
 import java.util.ArrayList;
-import java.util.Random;
 import java.util.concurrent.CountDownLatch;
 
 import lab9.task_9_1.bank.Account;
 import lab9.task_9_1.bank.BankAccountsManager;
 
 public class TransferManager {
-	
-	int accountsAmount;
+
 	ArrayList<Account> accounts;
-	BankAccountsManager accountManager;
+	BankAccountsManager accountsManager;
 	CountDownLatch barrier;
 
-	public TransferManager(int accountsAmount, ArrayList<Account> accounts, BankAccountsManager accountManager,
-			CountDownLatch barrier) {
-		this.accountsAmount = accountsAmount;
+	public TransferManager(ArrayList<Account> accounts, BankAccountsManager accountsManager, CountDownLatch barrier) {
 		this.accounts = accounts;
-		this.accountManager = accountManager;
+		this.accountsManager = accountsManager;
 		this.barrier = barrier;
 	}
 
-	public Runnable newTransfer() {
+	public Runnable newTransfer(Account from, Account to, int amount) {
 		return new Runnable() {
+			
 			@Override
 			public void run() {
-				Random random = new Random();
-				Account from = accounts.get(random.nextInt(accountsAmount));
-				Account to;
-				do {
-					to = accounts.get(random.nextInt(accountsAmount));
-				} while (from == to);
-				accountManager.tryConcurrentTransfer(from, to, random.nextInt(100));
+				accountsManager.tryConcurrentTransfer(from, to, amount);
 				barrier.countDown();
 			}
 		};
