b01b709dda3438d002b296b50a65714e883f739d
IvanOmelchenkoIP <io25112002@gmail.com>
2023-01-03 15:48:37 +0200

    Add simple CircularBuffer
    Add simple CircularBuffer implementation

--- java/src/lab9/task_9_2/buffer/ConcurrentCircularBuffer.java
+++ java/src/lab9/task_9_2/buffer/ConcurrentCircularBuffer.java
@@ -2,17 +2,25 @@ package lab9.task_9_2.buffer;
 
 public class ConcurrentCircularBuffer {
 	
-	final int elements;
+	final int size;
 	
-	public ConcurrentCircularBuffer(int elements) {
-		this.elements = elements;
+	String[] elements;
+	int writeIndex = 0;
+	int readIndex = 0;
+	
+	public ConcurrentCircularBuffer(int size) {
+		this.size = size;
+		this.elements = new String[size];
 	}
 	
 	public void add(String message) {
-		
+		elements[writeIndex++] = message;
+		if (writeIndex >= size) writeIndex = 0;
 	}
 	
 	public String take() {
-		return null;
+		String element = elements[readIndex++];
+		if (readIndex >= size) readIndex = 0;
+		return element;
 	}
 }
