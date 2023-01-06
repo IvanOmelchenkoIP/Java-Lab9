2fe09f0e63468db1a406d5c9ab96884c7b40de75
IvanOmelchenkoIP <io25112002@gmail.com>
2023-01-06 19:09:18 +0200

    Fix deadlock
    Fix deadlock with wrong indexes

--- java/src/lab9/task_9_2/buffer/ConcurrentCircularBuffer.java
+++ java/src/lab9/task_9_2/buffer/ConcurrentCircularBuffer.java
@@ -8,112 +8,11 @@ import java.util.concurrent.locks.ReentrantReadWriteLock.WriteLock;
 
 public class ConcurrentCircularBuffer {
 	
-	/*final int size;
-	int placesTaken = 0;
+	private final int bufferSize;
 	
-	String[] elements;
-	volatile int writeIndex = -1;
-	volatile int readIndex = -1;
-		
-	private final ReentrantLock bufferLock;
-	private final ReentrantLock freeLock;
-	private final Condition availableCondition;
-	private final Condition fullCondition;
-	
-	public ConcurrentCircularBuffer(int size) {
-		this.size = size;
-		this.elements = new String[size];
-		
-		this.bufferLock = new ReentrantLock();
-		this.freeLock = new ReentrantLock();
-		this.availableCondition = bufferLock.newCondition();
-		this.fullCondition = freeLock.newCondition();
-	}
-	
-	private boolean bufferEmpty() {
-		return readIndex == writeIndex || elements[readIndex] == null;
-	}
-	
-	private boolean bufferFull() {
-		return placesTaken == size;
-	}
-	
-	private int updateIndex(int index) {
-		index += 1;
-		if (index >= size) {
-			index = 0;
-		}
-		return index;
-	}
-	
-	
-	private void awaitBufferPlacesFreed() throws InterruptedException {
-		if (bufferFull()) {
-			bufferLock.unlock();
-			freeLock.lock();
-			try {
-				while (bufferEmpty()) {
-					fullCondition.await();
-				}
-				bufferLock.lock();
-			} finally {
-				freeLock.unlock();
-			}
-		}
-	}
-
-	public void put(String message) throws InterruptedException {
-		bufferLock.lock();
-		try {
-			writeIndex = updateIndex(writeIndex);
-			awaitBufferPlacesFreed();
-			elements[writeIndex] = message;
-			placesTaken += 1;
-			if (writeIndex == readIndex) {
-				availableCondition.signalAll();
-			}
-		} finally {
-			bufferLock.unlock();
-		}
-	}
-	
-	
-	private void awaitDataAvailable() throws InterruptedException {
-		if (bufferEmpty()) {
-			freeLock.unlock();
-			bufferLock.lock();
-			try {
-				while (bufferEmpty()) {
-					availableCondition.await();
-				}
-				freeLock.lock();
-			} finally {
-				bufferLock.unlock();
-			}
-		}
-	}
-	
-	public String take() throws InterruptedException {
-		freeLock.lock();
-		try {
-			readIndex = updateIndex(readIndex);
-			awaitDataAvailable();
-			placesTaken -= 1;
-			String element = elements[readIndex];
-			elements[readIndex] = null;
-			fullCondition.signalAll();
-			return element;
-		} finally {
-			freeLock.unlock();
-		}
-	}*/	
-	
-	final int bufferSize;
-	int placesTaken = 0;
-	
-	String[] elements;
-	volatile int writeIndex = -1;
-	volatile int readIndex = 0;
+	private String[] elements;
+	private volatile int tailIndex = -1;
+	private volatile int headIndex = 0;
 		
 	private final ReentrantReadWriteLock bufferLock;
 	private final ReadLock readLock;
@@ -131,19 +30,19 @@ public class ConcurrentCircularBuffer {
 	}
 	
 	private boolean bufferEmpty() {
-		return readIndex == writeIndex;
+		return headIndex == tailIndex || elements[headIndex % bufferSize]== null;
 	}
 	
 	private boolean bufferFull() {
-		return placesTaken == bufferSize;
+		return headIndex >= tailIndex;
 	}
 	
 	private void awaitDataAvailable() throws InterruptedException {
-		if (readIndex == writeIndex || elements[readIndex % bufferSize] == null) {
+		if (bufferEmpty()) {
 			readLock.unlock();
 			writeLock.lock();
 			try {
-				while (readIndex == writeIndex || elements[readIndex] == null) {
+				while (bufferEmpty()) {
 					dataAvailable.await();
 				}
 				readLock.lock();
@@ -156,10 +55,10 @@ public class ConcurrentCircularBuffer {
 	public void put(String message) throws InterruptedException {
 		writeLock.lock();
 		try {
-			++writeIndex;
-			elements[writeIndex % bufferSize] = message;
-			placesTaken += 1;
-			if (writeIndex == readIndex) {
+			int tmpTail = (tailIndex + 1) % bufferSize;
+			elements[tmpTail] = message;
+			tailIndex++;
+			if (tmpTail == (headIndex % bufferSize)) {
 				dataAvailable.signalAll();
 			}
 		} finally {
@@ -170,10 +69,10 @@ public class ConcurrentCircularBuffer {
 	public String take() throws InterruptedException {
 		readLock.lock();
 		try {
-			++readIndex;
 			awaitDataAvailable();
-			placesTaken -= 1;
-			return elements[readIndex % bufferSize];
+			String element = elements[headIndex % bufferSize];
+			headIndex++;
+			return element;
 		} finally {
 			readLock.unlock();
 		}
