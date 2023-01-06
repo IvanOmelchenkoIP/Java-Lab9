0547599e0460a142fb10cb2e8e2ed5492a556bb9
IvanOmelchenkoIP <io25112002@gmail.com>
2023-01-06 20:33:37 +0200

    Add awaiting for freeing buffer
    Add awaiting for freeing buffer in concurrent circular buffer

--- java/src/lab9/task_9_2/buffer/ConcurrentCircularBuffer.java
+++ java/src/lab9/task_9_2/buffer/ConcurrentCircularBuffer.java
@@ -14,31 +14,56 @@ public class ConcurrentCircularBuffer {
 	private volatile int tailIndex = -1;
 	private volatile int headIndex = 0;
 		
-	private final ReentrantReadWriteLock bufferLock;
+	private final ReentrantLock tailLock;
+	private final ReentrantLock headLock;
+	/*private final ReentrantReadWriteLock bufferLock;
 	private final ReadLock readLock;
-	private final WriteLock writeLock;
-	private final Condition dataAvailable;
+	private final WriteLock writeLock;*/
+	//private final Condition dataAvailable;
+	//private final Condition spaceFreed;
+	private final Condition bufferFreed;
+	private final Condition bufferReadable;
+
 	
 	public ConcurrentCircularBuffer(int bufferSize) {
 		this.bufferSize = bufferSize;
 		this.elements = new String[bufferSize];
 		
-		this.bufferLock = new ReentrantReadWriteLock();
+		/*this.bufferLock = new ReentrantReadWriteLock();
 		this.readLock = bufferLock.readLock();
 		this.writeLock = bufferLock.writeLock();
 		this.dataAvailable = writeLock.newCondition();
+		this.spaceFreed = writeLock.newCondition();*/
+		
+		this.tailLock = new ReentrantLock();
+		this.headLock = new ReentrantLock();
+		this.bufferFreed = headLock.newCondition();
+		this.bufferReadable = tailLock.newCondition();
 	}
 	
 	private boolean bufferEmpty() {
-		return headIndex == tailIndex || elements[headIndex % bufferSize]== null;
+		return headIndex == tailIndex || elements[headIndex % bufferSize] == null;
 	}
 	
 	private boolean bufferFull() {
-		return headIndex >= tailIndex;
+		return (tailIndex - headIndex) + 1 == bufferSize;
 	}
 	
-	private void awaitDataAvailable() throws InterruptedException {
+	private void awaitBufferReadable() throws InterruptedException {
 		if (bufferEmpty()) {
+			headLock.unlock();
+			tailLock.lock();
+			System.out.println(1);
+			try {
+				while (bufferEmpty()) {
+					bufferReadable.await();
+				}
+				headLock.lock();
+			} finally {
+				tailLock.unlock();
+			}
+		}
+		/*if (bufferEmpty()) {
 			readLock.unlock();
 			writeLock.lock();
 			try {
@@ -49,12 +74,53 @@ public class ConcurrentCircularBuffer {
 			} finally {
 				writeLock.unlock();
 			}
+		}*/
+	}
+	
+	private void awaitBufferFreed() throws InterruptedException {
+		if (bufferFull()) {
+			tailLock.unlock();
+			headLock.lock();
+			try {
+				while (bufferFull()) {
+					bufferFreed.await();
+				}
+				tailLock.lock();
+			} finally {
+				headLock.unlock();
+			}
 		}
+		/*if (bufferFull() ) {
+			readLock.lock();
+			writeLock.unlock();
+			try {
+				while(bufferFull()) {
+					spaceFreed.await();
+				}
+				writeLock.lock();
+			} finally {
+				readLock.unlock();
+			}
+		}*/
 	}
 	
 	public void put(String message) throws InterruptedException {
-		writeLock.lock();
+		tailLock.lock();
 		try {
+			awaitBufferFreed();
+			//awaitBufferFreed();
+			int tmpTail = (tailIndex + 1) % bufferSize;
+			elements[tmpTail] = message;
+			tailIndex++;
+			if (tmpTail == ((headIndex) % bufferSize)) {
+				bufferReadable.signalAll();
+			}
+		} finally {
+			tailLock.unlock();
+		}
+		/*writeLock.lock();
+		try {
+			awaitBufferFreed();
 			int tmpTail = (tailIndex + 1) % bufferSize;
 			elements[tmpTail] = message;
 			tailIndex++;
@@ -63,18 +129,33 @@ public class ConcurrentCircularBuffer {
 			}
 		} finally {
 			writeLock.unlock();
-		}
+		}*/
 	}
 	
 	public String take() throws InterruptedException {
-		readLock.lock();
+		headLock.lock();
+		try {
+			awaitBufferReadable();
+			String element = elements[headIndex % bufferSize];
+			headIndex++;
+			if (!bufferFull()) {
+				bufferFreed.signalAll();
+			}
+			return element;
+		} finally {
+			headLock.unlock();
+		}
+		/*readLock.lock();
 		try {
 			awaitDataAvailable();
 			String element = elements[headIndex % bufferSize];
 			headIndex++;
+			if (!bufferFull()) {
+				spaceFreed.signalAll();
+			}
 			return element;
 		} finally {
 			readLock.unlock();
-		}
+		}*/
 	}
 }
